e5855a2398f89dbcf4cb7210ad2f685550bfd036
Ilya Madyukov <ilya.madyukov@clearscale.net>
2015-11-02 11:32:29 +0000

    Updated db::create_ephemeral with a check if device already exists

--- cookbooks/db/recipes/create_ephemeral.rb
+++ cookbooks/db/recipes/create_ephemeral.rb
@@ -6,6 +6,13 @@ end
 
 NICKNAME = get_device_or_default(node, :device2, :nickname)
 
+  #A check if device already exists
+  mount_point = '/mnt/ephemeral'
+  platform = ::RightScale::Tools::Platform.factory
+  device_exists = platform.get_device_for_mount_point(mount_point)
+  log "#{device_exists} already exists at #{mount_point}" if device_exists
+  return if device_exists
+
 block_device NICKNAME do
     mount_point '/mnt/ephemeral'
     stripe_count node[:db][:ephemeral_stripe_count]
